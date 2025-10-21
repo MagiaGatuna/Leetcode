da850e64baf0d66e67878de93aa5b7e3ea7459ef
MagiCat <corinavicky07@gmail.com>
2025-10-20 22:26:21 -0400

    Time: 3 ms (18.94%), Space: 11.6 MB (12.8%) - LeetHub

---
+++ 0822-unique-morse-code-words/0822-unique-morse-code-words.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    unordered_map<char, string> claveMorse={
+            {'a', ".-"},
+        {'b', "-..."},
+        {'c', "-.-."},
+        {'d', "-.."},
+        {'e', "."},
+        {'f', "..-."},
+        {'g', "--."},
+        {'h', "...."},
+        {'i', ".."},
+        {'j', ".---"},
+        {'k', "-.-"},
+        {'l', ".-.."},
+        {'m', "--"},
+        {'n', "-."},
+        {'o', "---"},
+        {'p', ".--."},
+        {'q', "--.-"},
+        {'r', ".-."}, 
+        {'s', "..."}, 
+        {'t', "-"},    
+        {'u', "..-"},  
+        {'v', "...-"},
+        {'w', ".--"},  
+        {'x', "-..-"},
+        {'y', "-.--"}, 
+        {'z', "--.."}  
+        };
+    string morse(string s){
+        string result="";
+        for(int i=0; i<s.size(); i++){
+            result+=claveMorse[s[i]];
+        }
+        return result;
+    }
+
+    int uniqueMorseRepresentations(vector<string>& words) {
+        unordered_set<string> transformaciones;
+        if(words.size()==1) return 1;
+        for(int i=0; i<words.size(); i++){
+            transformaciones.insert(morse(words[i]));
+        }
+        return transformaciones.size();
+    }
+};
