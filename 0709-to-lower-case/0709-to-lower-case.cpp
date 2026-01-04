2f42f86ed872b1f9f5bf10a964bce686c24c9c33
MagiCat <corinavicky07@gmail.com>
2026-01-04 12:09:33 -0400

    Time: 0 ms (100%), Space: 8.3 MB (51.11%) - LeetHub

---
+++ 0709-to-lower-case/0709-to-lower-case.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    string toLowerCase(string s) {
+        for(int i=0; i<s.length(); i++){
+            if(s[i]<91 && s[i]>64){
+                s[i]=s[i]+32;
+            }
+        }
+        return s;
+    }
+};
