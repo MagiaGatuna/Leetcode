9d4dbf7ef5820657587849ee56f9bb039c8eb44b
MagiCat <corinavicky07@gmail.com>
2026-06-30 16:00:52 -0400

    Time: 288 ms (83.76%), Space: 0B (100%) - LeetHub

---
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -0,0 +1,6 @@
+-- Write your PostgreSQL query statement below
+SELECT E1.name
+FROM Employee AS E1
+INNER JOIN Employee AS E2 ON E1.id = E2.managerId
+GROUP BY E1.id, E1.name
+HAVING COUNT(E2.Id) >= 5
